d85a1c71defc985f55ce83645595d484f276be46
fire8327 <fire83274@gmail.com>
2026-03-30 02:02:26 +0300

    feat: icon

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- <Header/> -->
-    <main class="grid-container gap-y-16 py-16">
+    <main class="grid-container gap-y-16">
         <NuxtPage/>
     </main>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-    <p>123</p>
+    <div class="relative min-h-screen flex items-center justify-center overflow-hidden">
+        <div class="max-w-7xl mx-auto px-4 lg:px-8 py-20 text-center flex flex-col gap-8">
+            <div class="flex items-center gap-2 px-4 py-1.5 bg-zinc-800/50 backdrop-blur-sm rounded-full border border-zinc-700/50">
+                
+                <p class="text-sm text-zinc-300">Откройте редкое цифровое искусство</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
